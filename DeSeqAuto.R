bd926d1d0f02770077506ed77f0f8d83822dedad
DrFunke <n10648267@qut.edu.au>
2022-10-05 18:31:50 +1000

    upload

---
+++ DeSeqAuto.R
@@ -0,0 +1,105 @@
+## Gene-level differential expression analysis using DESeq2
+
+## Setup - Loading libraries
+########################################
+#For excel output
+#install.packages('writexl')
+#install.packages('ashr')
+library(writexl)
+library(DESeq2)
+
+
+output_file_list <- list(
+  ('3_500_500.xlsx'),
+  ('3_750_250.xlsx'),
+  ('3_1000_0.xlsx'),
+  ('6_500_500.xlsx'),
+  ('6_750_250.xlsx'),
+  ('6_1000_0.xlsx'),
+  ('9_500_500.xlsx'),
+  ('9_750_250.xlsx'),
+  ('9_1000_0.xlsx')
+)
+
+input_data_list <- list(
+  "./data/3_500_500.tsv",
+  "./data/3_750_250.tsv",
+  "./data/3_1000_0.tsv",
+  "./data/6_500_500.tsv",
+  "./data/6_750_250.tsv",
+  "./data/6_1000_0.tsv",
+  "./data/9_500_500.tsv",
+  "./data/9_750_250.tsv",
+  "./data/9_1000_0.tsv"
+)
+input_group_list <- list(
+  (data.frame(sampletype =c("condition1", "condition1", "condition1", "condition2", "condition2", "condition2"))),
+  (data.frame(sampletype =c("condition1", "condition1", "condition1", "condition2", "condition2", "condition2"))),
+  (data.frame(sampletype =c("condition1", "condition1", "condition1", "condition2", "condition2", "condition2"))),
+  (data.frame(sampletype =c("condition1", "condition1", "condition1", "condition1", "condition1", "condition1", "condition2", "condition2", "condition2", "condition2", "condition2", "condition2"))),
+  (data.frame(sampletype =c("condition1", "condition1", "condition1", "condition1", "condition1", "condition1", "condition2", "condition2", "condition2", "condition2", "condition2", "condition2"))),
+  (data.frame(sampletype =c("condition1", "condition1", "condition1", "condition1", "condition1", "condition1", "condition2", "condition2", "condition2", "condition2", "condition2", "condition2"))),
+  (data.frame(sampletype =c("condition1", "condition1", "condition1", "condition1", "condition1", "condition1", "condition1", "condition1", "condition1", "condition2", "condition2", "condition2", "condition2", "condition2", "condition2", "condition2", "condition2", "condition2"))),
+  (data.frame(sampletype =c("condition1", "condition1", "condition1", "condition1", "condition1", "condition1", "condition1", "condition1", "condition1", "condition2", "condition2", "condition2", "condition2", "condition2", "condition2", "condition2", "condition2", "condition2"))),
+  (data.frame(sampletype =c("condition1", "condition1", "condition1", "condition1", "condition1", "condition1", "condition1", "condition1", "condition1", "condition2", "condition2", "condition2", "condition2", "condition2", "condition2", "condition2", "condition2", "condition2")))
+)
+
+input_meta_list <- list(
+  ("3_meta.txt"),
+  ("3_meta.txt"),
+  ("3_meta.txt"),
+  ("6_meta.txt"),
+  ("6_meta.txt"),
+  ("6_meta.txt"),
+  ("9_meta.txt"),
+  ("9_meta.txt"),
+  ("9_meta.txt")
+)
+
+func <- function(input_data, input_group, output_file){
+  ####################################
+  ####Import data
+  ####################################
+  data <- read.table(input_data, header=T, row.names=1)
+  meta <- input_group
+  
+  #Create DESeq2Dataset object
+  dds <- DESeqDataSetFromMatrix(countData = data, colData = meta, design = ~ sampletype)
+  
+  ####################################
+  ####Normalizing the data
+  ####################################
+  
+  #generates size fator
+  dds <- estimateSizeFactors(dds)
+  #view normalization factor applied to each sample
+  sizeFactors(dds)
+  #normalised count matrix
+  normalized_counts <- counts(dds, normalized=TRUE)
+  
+  ########################################
+  #DE analysis: exploring the dispersion estimates and assessing model fit
+  ########################################
+  
+  ## Run analysis
+  dds <- DESeq(dds)
+  
+  #define contrast
+  #results of condition2 compared to condition1
+  contrast_oe <- c("sampletype","condition2","condition1")
+  
+  #Shrink data
+  results_table_unshrunken <- results(dds, contrast=contrast_oe, alpha = 0.05)
+  results_table <- lfcShrink(dds, contrast=contrast_oe, res=results_table_unshrunken, type="ashr")
+  
+  #View and write data
+  results_table <- data.frame(results_table)
+  #view(results_table)
+  write_xlsx(results_table, output_file)
+  
+}
+
+for (i in 1:9){
+  func(input_data_list[[i]],input_group_list[[i]],output_file_list[[i]])
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # IFN646_project
+Introduction
+
+The introduction of high-throughput RNA sequencing technologies has allowed us to understand the nature of gene expression. 
+These experiments would involve the quantification of a large number of short reads which are then aligned to a genome to create a count matrix of each gene or feature. 
+RNA differential expression tools allow the user to quickly find genes that are highly expressed or suppressed between two conditions.
+
+This project aims to compare tools that analyze differential expression of RNA sequencing data. 
+Synthetic count matrices were generated and provided for the comparison which included two conditional groups, condition 1 and condition 2, with varying sample sizes from 3 to 9 per group and approximately ten thousand genes. 
+A separate meta file was also provided that indicates whether condition 2 genes were either upregulated, downregulated or differentially expressed compared to condition 1.
+
+
+Method
+The differential expression tools we will investigate in this study include DESeq2, EdgeR and Voom limma. 
+The tools will be evaluated based on their ability to determine whether genes were differential expressed or not as indicated in the meta file. 
+
+To compare the ability to rank differentially expressed genes, we will generate receiver operating characteristic (ROC) graphs which plot true positive and false positive rates and evaluate each area under ROC curve (AUC) which calculates the performance of the model across all probability thresholds.
+RNA differential expression tools are built in the R language (version 4.2.1) and scripts were written in RStudio (version 2022.07.1 build 554). Results from these tools were analysed in Jupyter notebook (version 5.7.8)running Python version 3.7.3). 
+A logistic regression was trained using the output of the differential expression tool as the response variable and a meta data file as the predictor variable. Genes with a statistically significant fold change of p value=0.05 or smaller were considered differentially expressed.
+
+ 
+Results
+
+![alt text](/Picture500_500.png?raw=true)
+![alt text](/Picture750_250.png?raw=true)
+![alt text](/Picture1000_0.png?raw=true)
+ 
+Area under the ROC curve (AUC). Area under the ROC curve for DESeq2 and EdgeR using synthetic count data.
+Each file name indicates the number of genes that are upregulated and downregulated compared to in condition 2 compared to condition 1. 
+Sample size varies from n = 3, 6 or 9 per group.
+Each point represents an AUC score that has been generated from either DESeq2 (circle), EdgeR (square) or VoomLimma (Diamond).
+AUC score was increased in all scenarios when the sample size was increased.
